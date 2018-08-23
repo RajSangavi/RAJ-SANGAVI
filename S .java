af836a7ca37bf80701f831bb3eb0963220f846d3
RajSangavi <39334036+RajSangavi@users.noreply.github.com>
2018-08-23 10:50:25 +0530

    Create S .java

---
+++ S .java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class S {
+public static void main(String[] args)
+{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter a string");
+	String a=sc.nextLine();
+	//for(int i=0;i<a.length();i++)
+
+	{
+		System.out.println(a);
+	}
+}
+}
